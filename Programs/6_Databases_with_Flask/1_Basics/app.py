11dec927e1478ab035cec840cac6d3130db59cd6
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-12-29 18:50:11 +0530

    Complete Flask Migrate

--- Programs/6_Databases_with_Flask/1_Basics/app.py
+++ Programs/6_Databases_with_Flask/1_Basics/app.py
@@ -3,6 +3,9 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Used to migrate the database
+
+# ? Type set FLASK_APP=app.py in the terminal to set the environment variable
 
 # ? 2) get the path of the current file
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -19,6 +22,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+Migrate(app, db)  # Connects the app to the database
+
+# ? Type flask db init in the terminal to create the migrations folder
+# ? Type flask db migrate -m "Initial migration" in the terminal to create the migration file
+# ? Type flask db upgrade in the terminal to create the database
+
 
 # ? 4) Creating a model
 class Puppy(db.Model):
@@ -30,10 +39,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     # Used to represent the object
     def __repr__(self):
