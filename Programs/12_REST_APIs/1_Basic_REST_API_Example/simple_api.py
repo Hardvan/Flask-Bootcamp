c313315dc598206297fbe5c938865ef495e52a36
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2024-04-24 10:28:14 +0530

    Fine tune REST API

--- Programs/12_REST_APIs/1_Basic_REST_API_Example/simple_api.py
+++ Programs/12_REST_APIs/1_Basic_REST_API_Example/simple_api.py
@@ -2,17 +2,18 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)  # ? Used to add resources to the API.
+api = Api(app)  # ? Used to add resources to the API
 
 
-# ? Creating a resource.
+# ? Creating a resource
 class HelloWorld(Resource):
 
+    # ? GET request
     def get(self):
         return {"data": "Hello, World!"}
 
 
-# ? Adding the resource to the API.
+# ? Adding the resource to the API
 api.add_resource(HelloWorld, "/")
 
 if __name__ == '__main__':
--- Programs/12_REST_APIs/2_CRUD_REST_Basics/crud_api.py
+++ Programs/12_REST_APIs/2_CRUD_REST_Basics/crud_api.py
@@ -7,35 +7,48 @@ api = Api(app)
 puppies = []  # List of puppy dictionaries
 
 
+# ? Use Postman to test the API
+
+
 class PuppyNames(Resource):
 
     # ? READ
     def get(self, name):
 
+        print("=== In GET ===")
+
         for pup in puppies:
             if pup["name"] == name:
+                print(f"✅ Puppy ({name}) found")
                 return pup
 
+        # Puppy not found
+        print(f"❌ Puppy ({name}) not found")
         return {"name": None}, 404
 
     # ? CREATE
     def post(self, name):
 
+        print("=== In POST ===")
+
         pup = {"name": name}
         puppies.append(pup)
-
+        print(f"✅ Puppy ({name}) added")
         return pup
 
     # ? DELETE
     def delete(self, name):
 
+        print("=== In DELETE ===")
+
         for ind, pup in enumerate(puppies):
             if pup["name"] == name:
                 deleted_pup = puppies.pop(ind)
-                print(deleted_pup)
-
+                print(f"✅ Puppy ({name}) deleted")
                 return {"note": f"delete success (puppy name: {name})"}
 
+        # Puppy not found
+        print(f"❌ Puppy ({name}) not found")
         return {"note": "delete failed (puppy name not found)"}
 
 
